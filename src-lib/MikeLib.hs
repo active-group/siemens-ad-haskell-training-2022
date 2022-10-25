60c40d3581872fbafe4c5bd4d9d2a03c962e7868
Mike Sperber <sperber@deinprogramm.de>
2022-10-25 07:23:12 +0200

    Update.

--- src-lib/MikeLib.hs
+++ src-lib/MikeLib.hs
@@ -6,5 +6,21 @@ x = 10
 y :: Integer
 y = x + 10
 
+-- double a number
 f :: Integer -> Integer
+-- >>> f 12
+-- 24
 f n = n * 2
+
+-- design recipes
+
+-- data definition
+
+-- A pet is one of the following:
+-- - cat - OR -
+-- - dog - OR -
+-- - spider
+
+-- new data type
+data Pet = Cat | Dog | Spider
+  deriving Show
