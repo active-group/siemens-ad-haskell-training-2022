4c743839b16d872906f0025818fa8cd60818200d
Bianca Lutz <bianca.lutz@active-group.de>
2022-10-26 10:33:13 +0200

    Update.

--- src-lib/BiancaLib.hs
+++ src-lib/BiancaLib.hs
@@ -29,10 +29,10 @@ program:
 type Key = String
 type Value = Integer
 
-data DBCommand a =
-      Put Key Value (() -> DBCommand a)
-    | Get Key (Value -> DBCommand a)
-    | Done a
+data DBCommand a
+  = Put Key Value (() -> DBCommand a)
+  | Get Key (Value -> DBCommand a)
+  | Done a
 
 put :: Key -> Value -> DBCommand ()
 put key value = Put key value (\ () -> Done ())
@@ -101,3 +101,56 @@ p3' = do
 
 --
 
+{- yet another example: Teletype -}
+
+data TTYCommand a
+  = ReadTTY (String -> TTYCommand a)
+  | WriteTTY String (() -> TTYCommand a)
+  | ExitSuccess a
+
+readTTY :: TTYCommand String
+readTTY = ReadTTY (\ input -> ExitSuccess input)
+
+writeTTY :: String -> TTYCommand ()
+writeTTY output = WriteTTY output (\ () -> ExitSuccess ())
+
+exitTTY :: a -> TTYCommand a
+exitTTY result = ExitSuccess result
+
+spliceTTY :: TTYCommand a -> (a -> TTYCommand b) -> TTYCommand b
+spliceTTY (ReadTTY cb) next =
+    ReadTTY (\x -> spliceTTY (cb x) next)
+spliceTTY (WriteTTY output cb) next =
+    WriteTTY output (\x -> spliceTTY (cb x) next)
+spliceTTY (ExitSuccess result) next =
+    next result
+
+instance Functor TTYCommand where
+instance Applicative TTYCommand where
+
+instance Monad TTYCommand where
+    (>>=) = spliceTTY
+    return = ExitSuccess
+
+
+{- Apparently, there are some similarities:
+    + Both types have a "base case" that concludes a computation,
+      i.e., Done and ExitSuccess, and, thus, does not take a callback.
+    + All other constructors contain a callback that defines how the
+      computation should continue -- consequently, it is also called
+      a continuation.
+      This callback takes the result of the previous computation
+      and binds it to a local variable. Allowing subsequent computations
+      to refer to it by name.
+    + splice (aka bind) decends (recurses) into the callback until it reaches
+      the respective base case where it applies its second argument, i.e.,
+      the function that yields the subsequent computation(s).
+      In fact, the callback is the only sensible place where recursion might
+      occur since the callback is the only carrier of a self reference
+      (at the type level).
+
+    So, the mechanics used to "chain" multiple computation into a sequence
+    is basically the same. This common behaviour, and base case in conjunction
+    with a bind function that chains computation together, is what a monad
+    defines in an abstract way.
+-}
