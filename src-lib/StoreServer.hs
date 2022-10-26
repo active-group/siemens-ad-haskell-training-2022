68a1fb213282e30f72b6103a997fd4bfe3daae3d
Bianca Lutz <bianca.lutz@active-group.de>
2022-10-26 15:46:58 +0200

    Update.

--- src-lib/StoreServer.hs
+++ src-lib/StoreServer.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 {-# LANGUAGE GADTs, FlexibleContexts, PolyKinds, TypeOperators, DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
@@ -38,22 +37,41 @@ data Store m a where
     InsertValue :: Key -> Value -> Store m ()
     LookupKey :: Key -> Store m (Maybe Value)
 
+{- the following functions are pure boilerplate code
+   and could be generated with template haskell
+
+   one reason we decided against that in our code base
+   is the fact that this would hide certain facts;
+   the existence of functions named insertValue and
+   lookupKey for example. you would have to just know
+   that and could not infer it from the code alone.
+ -}
+
 insertValue :: Member Store r => Key -> Value -> Sem r ()
 insertValue key value = send (InsertValue key value)
 
 lookupKey :: Member Store r => Key -> Sem r (Maybe Value)
 lookupKey key = send (LookupKey key)
 
+-- making it more convenient to lookup keys (that might not exist)
 lookupKeyOrError ::
   Members
     '[ Store
-     , Polysemy.Error LookupError
+     , Polysemy.Error LookupError -- gives us Polysemy.throw
      ]
     r =>
   Key ->
   Sem r Value
 lookupKeyOrError key =
+    -- throw a (domain) error if the key does not exist
+    -- otherwise return the Value (unwrapping Just)
     maybe (Polysemy.throw NotFound) pure =<< lookupKey key
+    -- the execution is short-circuited, i.e. exited early,
+    -- if lookupKey yields Nothing
+    --
+    -- with this a caller would only ever see Value and does
+    -- not have to deal with Maybe Value cases like with the
+    -- implementation in BiancaLib (see makeJohannesYounger)
 
 replaceValue :: Sqlite.Connection -> Key -> Value -> IO ()
 replaceValue conn key value =
@@ -91,11 +109,69 @@ createDbTable conn =
 
 -- Our web API as a type.
 
-type LookupRoute = Capture "key" Key :> Get '[JSON] Value
+type Routes =
+  -- GET /:key/ ---> LookupKey
+  -- response: Value
+  -- error response in case HTTP Not Found (404)
+  Capture "key" Key :> Get '[JSON] Value
+  -- PUT /:key/:value/ ---> InsertValue
+  -- response: HTTP No Content (204)
+  :<|> Capture "key" Key :> Capture "value" Value :> PutNoContent
 
-type InsertRoute = Capture "key" Key :> ReqBody '[JSON] Value :> PutNoContent
+{- Some combinators Servant defines:
 
-type Routes = LookupRoute :<|> InsertRoute
+  -- URL parameter, e.g. /:key/
+  --      tag (*)   (haskell) type
+  Capture "key"     Key
+  -- (*): tags are used to distinguish multiple segments with the same type
+  -- Capture translates into an input parameter of the corresponding handler
+
+  -- Request body, i.e. expected payload
+  -- Usually for more complex types that you cannot pass as URL segments directly
+  --      Content-Type(s)   (haskell) type
+  ReqBody '[JSON]           Value
+  -- ReqBody translates into an input parameter of the corresponding handler
+
+  -- Response type
+  -- HTTP Verb  Content-Type(s)     (haskell) type
+     Get        '[JSON, PlainText]  Value
+  -- There are other combinators for the various HTTP verbs, e.g. Put, Post.
+  -- There are also special forms for some Verbs:
+  -- e.g. 'PutNoContent' means PUT request responding with HTTP response code 204
+-}
+
+{- you can group routes with a common prefix: -}
+type Routes' =
+  Capture "key" Key
+    :> ( Get '[JSON] Value
+          :<|> Capture "value" Value :> ReqBody '[JSON] Value :> PutNoContent
+       )
+
+{- You can split your API into "sub APIs" -}
+
+type LookupRoute =
+  -- GET localhost:8080/:key/
+  Capture "key" Key :> Get   '[JSON]        Value
+
+type InsertRoute =
+  -- PUT localhost:8080/:key/:value/
+  -- payload: none
+  Capture "key" Key :> Capture "value" Value :> PutNoContent
+
+{- ... and combine these "sub APIs" into more complex APIs: -}
+type Routes'' = LookupRoute :<|> InsertRoute -- equivalent to Routes
+
+
+-- supplying the value for our insert route
+-- using request body instead of a URL parameter/segment:
+type InsertRoute' =
+  -- PUT localhost:8080/:key/
+  -- payload: JSON encoded Value
+  Capture "key" Key :> ReqBody '[JSON] Value :> PutNoContent
+-- Note: InsertRoute and InsertRoute' result in the same
+--       type signature that a handler would need:
+--          handlerInsert :: Key -> Value -> Sem r NoContent
+
 
 -- Serving our API.
 server ::
@@ -106,11 +182,30 @@ server ::
      ]
     r =>
   ServerT Routes (Sem r)
-server = lookupKeyOrError :<|> putNoContent
-  where
-    putNoContent key value = insertValue key value >> pure NoContent
+server =
+  -- here our handler functions are bound to different parts of the API
+  lookupKeyOrError  -- sometime you can use some effect based computation directly
+  :<|> putNoContent -- sometimes you need to tweak it a bit to return the expected type
+  -- Note: the structure of the server exactly matches the API definition
+  --       You even use the same combinators
 
--- Transform computations in "our" Sem monad into computations
+  -- grouping make a difference:
+  -- if our server was of Type ServerT Routes' (Sem r), we'd need:
+  -- \ key -> (lookupKeyOrError key :<|> putNoContent key)
+
+  where
+    putNoContent :: Member Store r => Key -> Value -> Sem r NoContent
+    putNoContent key value =
+      insertValue key value -- call the InsertValue "method" of our Store "interface"
+        >> pure NoContent   -- ... and ignoring its result, as it is unit (think: void) anyway
+                            --     return the Servant value NoContent, i.e. response code 204 without payload
+
+{- Everything that follows you usually have to figure out only once.
+   In particular, with respect to our code base, you will rarely have
+   to touch it, if at all.
+ -}
+
+-- Turn computations in "our" Sem monad into computations
 -- running in Servant's Handler monad.
 transformToHandler ::
   FilePath ->
@@ -126,8 +221,14 @@ transformToHandler dbfile sem =
   Sqlite.withConnection dbfile (run sem)
     & liftIntoHandler
   where
+    -- running the Sem computation, i.e. "scraping" off all effects
+    -- by supplying appropriate interpreters/runners
     run request conn =
       request
+-- the one piece that might change due to changes to the API:
+-- E.g. if you'd use a handler that depends on an effect that
+-- was not used before, you would have to extend the list of
+-- interpreters accordingly
         & runStoreAsSqlite conn
         & Polysemy.mapError toServerError
         & Polysemy.runError @ServerError
@@ -141,6 +242,8 @@ transformToHandler dbfile sem =
 mkWaiApplication :: FilePath -> Application
 mkWaiApplication db = serve (Proxy @Routes) server'
   where
+    -- translate the Sem computation into something Servant expects,
+    -- that is a computation running in Servant's Handler monad.
     server' = hoistServer (Proxy @Routes) (transformToHandler db) server
 
 -- Our server's "main" function
